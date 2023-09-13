09b6ee1668a4b84582b5869b762d04595d9332d2
Caglar Demir <caglardemir8@gmail.com>
2023-09-13 14:32:10 +0200

    Jaccard Sim added

---
+++ core/quality_funcs.py
@@ -0,0 +1,13 @@
+from typing import Set
+def jaccard_similarity(y: Set[str], yhat: Set[str]) -> float:
+    """
+    Compute Jaccard Similarity
+    :param y: A set of URIs
+    :param yhat: A set of URIs
+    :return:
+    """
+    if len(yhat) == len(y) == 0:
+        return 1.0
+    if len(yhat) == 0 or len(y) == 0:
+        return 0.0
+    return len(y.intersection(yhat)) / len(y.union(yhat))
